48ce134b8c23a458412fff8f4fab8e70dc3120ec
Fuad Khan <fkhan@whitestar.com>
2020-12-16 11:00:38 -0700

    Fixed destination creation

--- BLMsort/sort.py
+++ BLMsort/sort.py
@@ -2,27 +2,26 @@ import os
 import shutil
 import os.path
 
-srcpath = r"D:\OH\unsorted"
+srcpath = r"./unsorted"
 srcfiles = os.listdir(srcpath)
 
-destpath = r"D:\OH\sorted"
+destpath = r"./sorted"
 
 # extracts the twnshp/range from filenames and filter out duplicates
-destdirs = list(set([filename[6:13] for filename in srcfiles]))
+#destdirs = list(set([filename[6:13] for filename in srcfiles]))
+for srcfile in srcfiles:
+    state = srcfile[:2]
+    meridian = srcfile[2:4]
+    townrange = srcfile.split('_')[1]
 
+    dest = os.path.join(destpath, state, meridian, townrange)
 
-def create(dirname, destpath):
-    full_path = os.path.join(destpath, dirname)
-    if os.path.isdir(full_path):
-        os.mkdir(full_path)
-    return full_path
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+    
+    filename = os.path.basename(srcfile)
 
-def move(filename, dirpath):shutil.move(os.path.join(srcpath, filename),dirpath)
-
-# create destination directories and store their names along with full paths
-targets = [(folder, create(folder, destpath)) for folder in destdirs]
-
-for dirname, full_path in targets:
-    for filename in srcfiles:
-        if dirname == filename[0:2]:
-            move(filename, full_path)d
+    # add filename to destination path
+    dest = os.path.join(dest, filename)
+    src = os.path.join(srcpath, srcfile)
+    os.rename(src, dest)    
