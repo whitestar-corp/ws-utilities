13e118aabe3588dd8dbc39654e914c81abd05989
Fuad Khan <fkhan@whitestar.com>
2021-08-16 14:24:53 -0700

    Add Pipelines TXRRC

---
+++ TXRCC Scraper/piplines/get_pipe.bat
@@ -0,0 +1,4 @@
+call activate arcgispro-py3-clone 
+python "D:\Users\rwhite\Documents\texas-wells\piplines\python-scripts\get_pipe.py"
+
+
---
+++ TXRCC Scraper/piplines/python-scripts/get_pipe.py
@@ -0,0 +1,59 @@
+import time, os, tempfile, sys, zipfile, io
+from selenium import webdriver
+from datetime import date, timedelta
+from selenium.webdriver.support.ui import WebDriverWait
+
+dr = r"D:\Users\rwhite\Downloads"
+files = os.listdir(dr)
+old_pipe_data = [file for file in files if file.startswith("documents")]
+for file in old_pipe_data:
+    path_to_file = os.path.join(dr, file)
+    os.remove(path_to_file)
+
+url = 'https://mft.rrc.texas.gov/link/'
+page = ['d4eda8c4-9ff0-43b7-8f19-da0a57f10fd2']
+executable_path = r'D:\Users\rwhite\Downloads\chromedriver.exe'
+element = ['//*[@id="fileTable:j_id__v_1"]/option[4]',
+           '//*[@id="fileTable:j_id_1q"]/div/div[2]',
+           '//*[@id="j_id_3a:j_id_3a"]/span']
+targetdir = r'D:\Users\rwhite\Documents\texas-wells\piplines\data'
+
+def every_downloads_chrome(driver):
+    if not driver.current_url.startswith("chrome://downloads"):
+        driver.get("chrome://downloads/")
+    return driver.execute_script("""
+        var items = document.querySelector('downloads-manager')
+            .shadowRoot.getElementById('downloadsList').items;
+        if (items.every(e => e.state === "COMPLETE"))
+            return items.map(e => e.fileUrl || e.file_url);
+        """)
+
+def extract(filepath, targetdir):
+    z = zipfile.ZipFile(filepath)
+    for f in z.namelist():
+        dirname = os.path.splitext(f)[0]  
+        content = io.BytesIO(z.read(f))
+        zip_file = zipfile.ZipFile(content)
+        for i in zip_file.namelist():
+            zip_file.extract(i, targetdir)
+
+def scrapeRRC(page=page, element=element, executable_path=executable_path):
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(executable_path, options=options)
+    driver.get(url + page[0])
+    driver.implicitly_wait(5)
+    driver.find_element_by_xpath(element[0]).click()
+    time.sleep(4)
+    driver.find_element_by_xpath(element[1]).click()
+    time.sleep(4)
+    driver.find_element_by_xpath(element[2]).click()
+    try:
+        paths = WebDriverWait(driver, 10800).until(every_downloads_chrome)
+    finally:
+        driver.quit()
+    return paths[0][8:]
+
+if __name__ == '__main__':
+    
+    path = scrapeRRC()
+    extract(path, targetdir)
